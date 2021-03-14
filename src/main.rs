62f6edee482e054e998399adbbb4d491eb02853c
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-03-14 19:39:19 +0100

    Listen socket bind to /tmp/rust-uds.sock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io::{BufRead, BufReader};
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+
+fn handle_client(stream: UnixStream) {
+    // Add Buffer to stream
+    let stream = BufReader::new(stream);
+    for line in stream.lines() {
+        println!(
+            "{}",
+            line.unwrap_or_else(|error| {
+                panic!(format!(
+                    "learning-socket.main.handle_client: Could not read line from stream {:?}",
+                    error
+                ))
+            })
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = UnixListener::bind("/tmp/rust-uds.sock").unwrap_or_else(|error| {
+        panic!(format!(
+            "learning-socket.main: Could not bind to socket. {:?}",
+            error
+        ))
+    });
+    for stream in listener.incoming() {
+        stream.map_or_else(
+            |error| {
+                println!(
+                    "{:?}",
+                    format!(
+                        "learning-socket.main: Could not listen socket incoming {:?}",
+                        error
+                    )
+                );
+            },
+            |stream| {
+                println!("Got connection request");
+                thread::spawn(|| handle_client(stream));
+            },
+        )
+    }
 }
